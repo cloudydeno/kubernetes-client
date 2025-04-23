b0b446157cf2dd702d5b76ea5962aa6cfb626abb
Daniel Lamando <dan@danopia.net>
2025-04-23 10:17:06 +0200

    Update deps_jsr.ts

--- deps_jsr.ts
+++ deps_jsr.ts
@@ -1,4 +1,8 @@
-export { join as joinPath } from "jsr:@std/path@1.0.6";
+export {
+  dirname,
+  join as joinPath,
+  resolve as resolvePath,
+} from "jsr:@std/path@1.0.6";
 
 export { parse as parseYaml } from "jsr:@std/yaml@1.0.5";
 
