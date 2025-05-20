5fb65de34320a429826910e2e51253471cf95690
Daniel Lamando <dan@danopia.net>
2025-05-20 23:50:02 +0200

    Update JSR publication, better deps_jsr hack

--- tunnel-beta/via-websocket.ts
+++ tunnel-beta/via-websocket.ts
@@ -1,5 +1,5 @@
-import { map } from "https://deno.land/x/stream_observables@v1.3/transforms/map.ts";
-import { EOF, external } from "https://deno.land/x/stream_observables@v1.3/sources/external.ts";
+import { map } from "jsr:@cloudydeno/stream-observables@1.4.1/transforms/map.ts";
+import { EOF, external } from "jsr:@cloudydeno/stream-observables@1.4.1/sources/external.ts";
 
 import { JSONValue, KubernetesTunnel, RequestOptions } from "../lib/contract.ts";
 import { KubeConfigRestClient } from "../transports/via-kubeconfig.ts";
